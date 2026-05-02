e5cd7798a7b0334bb0d75e51e42189be4b5a72e2
Nicholas Francis O'Brien <nicholasob972@gmail.com>
2026-05-02 18:25:38 +1200

    Hide Svelte announcer under CSP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,3 +28,17 @@
 </svelte:head>
 
 {@render children()}
+
+<style>
+	:global(#svelte-announcer) {
+		position: absolute;
+		left: 0;
+		top: 0;
+		clip: rect(0 0 0 0);
+		clip-path: inset(50%);
+		width: 1px;
+		height: 1px;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+</style>
