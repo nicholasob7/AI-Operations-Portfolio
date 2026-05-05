d4e234e71433191a93eb2736c6bbc05cc2d62e5f
Nicholas Francis O'Brien <101837624+nicholasob7@users.noreply.github.com>
2026-05-05 18:21:57 +1200

    Self-host Spectral font assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { toCanonicalUrl } from '$lib/site';
+	import './fonts.css';
 	import './layout.css';
 
 	let { children } = $props();
@@ -19,12 +20,6 @@
 	<meta name="theme-color" content="#060912" />
 	<link rel="icon" type="image/svg+xml" sizes="any" href="/favicon-n-purple-green.svg?v=1" />
 	<link rel="shortcut icon" href="/favicon-n-purple-green.svg?v=1" />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Spectral:wght@400;500;600;700&display=swap"
-		rel="stylesheet"
-	/>
 </svelte:head>
 
 {@render children()}
